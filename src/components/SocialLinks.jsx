6c163dfd1ba077a68cc3de603a98badbe9a00294
Ash19-88 <ayelenllampa88@gmail.com>
2023-07-26 12:08:43 -0300

    updating resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill, BsWhatsapp} from 'react-icons/bs';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill, BsWhatsapp } from "react-icons/bs";
 
 const SocialLinks = () => {
   const links = [
     {
       id: 1,
       child: (
-        <>WhatsApp <BsWhatsapp size={30} /></>
+        <>
+          WhatsApp <BsWhatsapp size={30} />
+        </>
       ),
       href: "https://wa.link/mdejk5",
       style: "rounded-tr-md ",
@@ -16,54 +18,68 @@ const SocialLinks = () => {
     {
       id: 2,
       child: (
-        <>LinkedIn <FaLinkedin size={30} /></>
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
       ),
-      href: 'https://www.linkedin.com/in/ayelen-llampa1988',
+      href: "https://www.linkedin.com/in/ayelen-llampa1988",
     },
-     {
+    {
       id: 3,
       child: (
-        <>GitHub <FaGithub size={30} /></>
+        <>
+          GitHub <FaGithub size={30} />
+        </>
       ),
-      href: 'https://github.com/Ash19-88',
+      href: "https://github.com/Ash19-88",
     },
-      {
+    {
       id: 4,
       child: (
-        <>Mail <HiOutlineMail size={30} /></>
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
       ),
-      href: 'mailto: ayelenllampa88@gmail.com',
+      href: "mailto: ayelenllampa88@gmail.com",
     },
-       {
+    {
       id: 5,
       child: (
-        <>Resume <BsFillPersonLinesFill size={30} /></>
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
       ),
-      href: '/Ashresume.pdf',
+      href: "/Ayelen-Llampa-resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
-  ]
+  ];
   return (
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        {links.map(({ id, child, href, style, download}) => (
-          <li 
+        {links.map(({ id, child, href, style, download }) => (
+          <li
             key={id}
-            className={  "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-           " " +
-           style}>
-            <a href={href}
-              className='flex justify-between items-center w-full text-white'
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
               download={download}
               target="_blank"
               rel="noreferrer"
             >
-          {child}</a></li>
+              {child}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
