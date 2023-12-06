fe0d0f55e6e6dd54b00f0647c47a90644ded524c
Ash19-88 <ayelenllampa88@gmail.com>
2023-12-05 21:36:01 -0300

    integrating translations in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
+import { useTranslation } from "react-i18next";
 import myphoto from "../assets/myphoto.png";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-scroll";
 
 const Home = () => {
+  const { t } = useTranslation("home");
+
   return (
     <div className="bg-black font-Raleway">
       <div
@@ -12,10 +15,10 @@ const Home = () => {
         <div className="max-w-screen-lg mx-auto flex flex-col-reverse justify-center items-center md:h-full px-4 md:flex-row">
           <div className="flex flex-col justify-center h-full py-10 md:mx-16">
             <h2 className="text-4xl sm:text-6xl font-bold text-white ">
-              Building beautiful web experiences
+              {t("homeTitle")}
             </h2>
             <span className="text-slate-300 py-4 max-w-md font-medium tracking-widest">
-              I invite you to know more about me and what I do
+              {t("homeSubtitle")}
             </span>
             <div>
               <Link
@@ -25,7 +28,7 @@ const Home = () => {
                 className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-gray-500 
            font-semibold to-slate-700 cursor-pointer tracking-widest"
               >
-                Projects{" "}
+                {t("projects")}
                 <span className="ml-2 group-hover:rotate-90 duration-300">
                   <BsFillArrowRightCircleFill size={25} />
                 </span>
--- src/i18n.js
+++ src/i18n.js
@@ -7,6 +7,9 @@ import navbar_pt from "./locales/navbar/navbar_pt.json";
 import about_es from "./locales/about/about_es.json";
 import about_en from "./locales/about/about_en.json";
 import about_pt from "./locales/about/about_pt.json";
+import home_es from "./locales/home/home_es.json";
+import home_en from "./locales/home/home_en.json";
+import home_pt from "./locales/home/home_pt.json";
 
 i18n
   .use(LanguageDetector)
@@ -21,14 +24,17 @@ i18n
       es: {
         navbar: navbar_es,
         about: about_es,
+        home: home_es,
       },
       en: {
         navbar: navbar_en,
         about: about_en,
+        home: home_en,
       },
       pt: {
         navbar: navbar_pt,
         about: about_pt,
+        home: home_pt,
       },
     },
   });
